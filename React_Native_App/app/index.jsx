d79ef7c92d45ca27f3baa0fae14e3051435d24d8
AnjanaTinush <146495563+AnjanaTinush@users.noreply.github.com>
2024-07-04 20:18:25 +0530

    create navigation bar

--- React_Native_App/app/index.jsx
+++ React_Native_App/app/index.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white"r>
       <Text className="text-3xl font-pblack">Hellow world!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{color:'blue'}}>Go to profile</Link>
+      <Link href="/home" style={{color:'blue'}}>Home</Link>
     </View>
   );
 }
