577a09d3ca78f4162a7ef903e696ad2e987e95c1
AnjanaTinush <146495563+AnjanaTinush@users.noreply.github.com>
2024-07-04 19:25:59 +0530

    create folder structure

--- React_Native_App/app/index.jsx
+++ React_Native_App/app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white"r>
-      <Text className="text-3xl">Hellow world!</Text>
+      <Text className="text-3xl font-pblack">Hellow world!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{color:'blue'}}>Go to profile</Link>
     </View>
