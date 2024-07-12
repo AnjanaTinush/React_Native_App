07222893eddb3b52b257bd3819c015929e536413
AnjanaTinush <146495563+AnjanaTinush@users.noreply.github.com>
2024-07-12 21:06:55 +0530

    debuging full project with vertion

---
+++ React_Native_App/aora/lib/appwrite.js
@@ -0,0 +1,91 @@
+import { Account, Avatars, Client, Databases, ID, Query } from 'react-native-appwrite';
+
+export const config = {
+    endpoint: 'https://cloud.appwrite.io/v1',
+    platform: "com.jsm.aora",
+    projectId: '668d659b001320fc5df3',
+    databaseId: '668d67de000cc8deb989',
+    userCollectionId: '668d685d001e6f58beb0',
+    videoCollectionId: '668d6893002fd2e72834',
+    storageId: '668d6c15003a02c738c6'
+}
+
+// Init your React Native SDK
+const client = new Client();
+
+client
+    .setEndpoint(config.endpoint) // Your Appwrite Endpoint
+    .setProject(config.projectId) // Your project ID
+    .setPlatform(config.platform) // Your application ID or bundle ID.
+    ;
+
+const account = new Account(client);
+const avatars = new Avatars(client);
+const databases = new Databases(client);
+
+export const createUser = async (email, password, username) => {
+    try {
+        const newAccount = await account.create(
+            ID.unique(),
+            email,
+            password,
+            username
+        )
+
+        if (!newAccount) throw Error;
+
+        const avatarUrl = avatars.getInitials(username)
+
+        await signIn(email, password);
+
+        const newUser = await databases.createDocument(
+            config.databaseId,
+            config.userCollectionId,
+            ID.unique(),
+            {
+                accountId: newAccount.$id,
+                email,
+                username,
+                avatar: avatarUrl
+            }
+        )
+
+        return newUser;
+
+    } catch (error) {
+        console.log(error);
+        throw new Error(error);
+    }
+}
+
+export async function signIn(email, password) {
+    try {
+      const session = await account.createEmailSession(email, password);
+  
+      return session;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+export const getCurrentUser = async () => {
+    try {
+
+        const currentAccount  = await account.get();
+
+        if(!currentAccount) throw Error;
+
+        const currentUser = await databases.listDocuments(
+            config.databaseId,
+            config.userCollectionId,
+            [Query.equal('accountId', currentAccount.$id)]
+        )
+
+        if(!currentUser) throw Error;
+
+        return currentUser.documents[0];
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
